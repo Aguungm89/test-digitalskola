1c9356424f62a98bd06f2810f7a2af58496b1082
Aguungm89 <89384431+Aguungm89@users.noreply.github.com>
2022-04-08 21:28:02 +0700

    update

--- etl.py
+++ etl.py
@@ -1,10 +1,5 @@
 class ELT:
-<<<<<<< HEAD
-
     def __init__(self) -> None:
         print("tambahan")
         pass
-=======
-    def __init__(self) -> None:
-        print("tambahan")
->>>>>>> main
+
